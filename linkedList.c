c1efa9ae84014817912dc01c2811d16d164bc57d
1801607 <e1801607@shell.vamk.fi>
2018-10-29 16:00:22 +0200

    additional functions

--- linkedList.c
+++ linkedList.c
@@ -1,13 +1,19 @@
 #include "linked_list.h"
-// create a new list item and add that to end of list int 
-add_to_list(linked_list *ll, char * s) {
+
+
+
+// create a new list item and add that to end of list
+int add_to_list(linked_list *ll, char * s)
+{
 	linked_list * node, *p;
+
 	// allocate a memory
 	node = (linked_list *)malloc(sizeof(struct linked_list));
 	if (node == NULL)
 	{
 		return -1;
 	}
+
 	// add node into list
 	node->data = s;
 	node->next = NULL;
@@ -17,14 +23,21 @@ add_to_list(linked_list *ll, char * s) {
 	node->index = index;
 	return index;
 }
-// displays the required element int display_item(linked_list *ll) {
+
+// displays the required element
+int display_item(linked_list *ll)
+{
 	if(!ll) return -1;
 	else{
-		printf("\nIndex %d: %s\n", ll->index, ll->data);
+		printf("\nIndex %d:  %s\n", ll->index, ll->data);
 		return 0;
 	}
 }
-// list all element in a list int display_list(linked_list *ll) {
+
+
+// list all element in a list
+int display_list(linked_list *ll)
+{
 	int index = 0;
 	linked_list *p;
 	for (p = ll; p->next != NULL; p = p->next, index++)
@@ -35,8 +48,10 @@ add_to_list(linked_list *ll, char * s) {
 	printf("\n");
 	return index;
 }
-// search for data from a list linked_list * 
-search_from_list(linked_list *ll, char *s) {
+
+// search for data from a list
+linked_list * search_from_list(linked_list *ll, char *s)
+{
 	
 	linked_list *p;
 	for (p = ll; p->next != NULL; p = p->next)
@@ -48,11 +63,15 @@ search_from_list(linked_list *ll, char *s) {
 		}
 	}
 	return NULL;
+
 }
-// delete item from a list and free space int 
-delete_from_list(linked_list *ll, int index) {
+
+// delete item from a list and free space
+int delete_from_list(linked_list *ll, int index)
+{
 	linked_list *p, *q;
 	int i;
+
 	// find the index-th element
 	i = 0;
 	for (p = ll; p->next != NULL; p = p->next)
@@ -70,5 +89,116 @@ delete_from_list(linked_list *ll, int index) {
 	q = p->next->next;
 	free(p->next);
 	p->next = q;
+
 	return linkedlist_status(ll);
 }
+
+// remove all items from the list and free memory
+int empty_list(linked_list *ll)
+{
+	linked_list *p, *q;
+	int num;
+
+	num = 0;
+	p = ll->next;
+	// empty list
+	if (p == NULL)
+	{
+		return 0;
+	}
+	while (1)
+	{
+		q = p->next;
+		// free the space
+		free(p);
+		num++;
+		if (q == NULL)
+		{
+			break;
+		}
+		p = q;
+	}
+	return num;
+}
+
+// swap order of two items
+int swap_items(linked_list *ll, linked_list *f, linked_list * s)
+{
+	linked_list *pf, *ps, *tf, *ts;
+
+	// find the piror of the f and s
+	for (pf = ll; pf->next != NULL; pf = pf->next)
+	{
+		if (pf->next == f)
+		{
+			break;
+		}
+	}
+	for (ps = ll; ps->next != NULL; ps = ps->next)
+	{
+		if (ps->next == s)
+		{
+			break;
+		}
+	}
+	if (pf->next != f || ps->next != s)
+	{
+		return -1;
+	}
+
+	// exchange
+	tf = f->next;
+	ts = s->next;
+	pf->next = s;
+	ps->next = f;
+	s->next = tf;
+	f->next = ts;
+
+
+	return 0;
+}
+
+// sort list in rising order based on data
+int sort_list(linked_list *ll)
+{
+	linked_list *q, *t;
+	int end;
+	if (ll->next == NULL || ll->next->next == NULL)
+	{
+		return 0;
+	}
+	// bubble sort
+	do
+	{
+		end = 1;
+		for (q = ll; q->next->next != NULL; q = q->next)
+		{
+			if (strcmp(q->next->data, q->next->next->data) > 0)
+			{
+				t = q->next->next;
+				q->next->next = t->next;
+				t->next = q->next;
+				q->next = t;
+				end = 0;
+			}
+		}
+	} while (end != 1);
+
+	return 0;
+}
+
+// count the number of elements in the list
+int linkedlist_status(linked_list *ll)
+{
+	linked_list *p;
+	int i;
+	i = 0;
+	for (p = ll; p->next != NULL; p = p->next)
+	{
+		// count increase
+		i++;
+	}
+	return i;
+}
+
+
